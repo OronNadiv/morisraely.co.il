7d23ac48edf28ff939155e6b4a05646491054e1d
Oron Nadiv <oron@nadiv.us>
2019-02-19 20:49:11 -0800

    fix js error

--- public/js/custom.js
+++ public/js/custom.js
@@ -53,38 +53,6 @@
         });
     },
 
-    //Funfacts
-    ElvishApp.prototype.initFunFacts = function() {
-        var a = 0;
-        $(window).on('scroll',function() {
-            var oTop = $('#counter').offset().top - window.innerHeight;
-            if (a == 0 && $(window).scrollTop() > oTop) {
-                $('.lan_fun_value').each(function() {
-                    var $this = $(this),
-                        countTo = $this.attr('data-count');
-                    $({
-                        countNum: $this.text()
-                    }).animate({
-                            countNum: countTo
-                        },
-                        {
-                            duration: 2000,
-                            easing: 'swing',
-                            step: function() {
-                                $this.text(Math.floor(this.countNum));
-                            },
-                            complete: function() {
-                                $this.text(this.countNum);
-                                //alert('finished');
-                            }
-
-                        });
-                });
-                a = 1;
-            }
-        });
-    },
-
     //Portfolio Filter
     ElvishApp.prototype.initPortfolioFilter = function() {
         $(window).on('load', function () {
@@ -187,7 +155,6 @@
         this.initNavbarStickey();
         this.initNavbarSmooth();
         this.initNavbarScrollSpy();
-        this.initFunFacts();
         this.initPortfolioFilter();
         this.initMfpImages();
         this.initClientSlider();
